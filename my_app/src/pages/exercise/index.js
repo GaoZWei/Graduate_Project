cce1ddfa31029c831299d81934d4f83109a845ee
Gao <1520045270@qq.com>
2019-02-13 20:43:14 +0800

    添加按钮

--- my_app/src/pages/exercise/index.js
+++ my_app/src/pages/exercise/index.js
@@ -12,34 +12,128 @@ class Exercise extends Component {
           这是健身动作库主页
           <Row>
             <Col span={6}>
-              左侧筛选栏
               <div style={{ marginTop: 15 }}>
+                器械要求
                 <Row gutter={16}>
-                  <Col className="gutter-row" span={6}>
-                    <Button size='small' type="primary">不限器械</Button>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      不限器械
+                    </Button>
                   </Col>
-                  <Col className="gutter-row" span={6}>
-                    <Button size='small' type="primary">徒手训练</Button>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      徒手训练
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      双人训练
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      其他器械
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+
+              <div style={{ marginTop: 15 }}>
+                自由器械
+                <Row gutter={16}>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      哑铃
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      杠铃
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      壶铃
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      弹力带
+                    </Button>
                   </Col>
                 </Row>
               </div>
               <div style={{ marginTop: 15 }}>
                 <Row gutter={16}>
-                  <Col className="gutter-row" span={6}>
-                    <Button size='small' type="primary">哑铃</Button>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      瑞士球
+                    </Button>
                   </Col>
-                  <Col className="gutter-row" span={6}>
-                    <Button  size='small'  type="primary">杠铃</Button>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      泡沫轴
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      波速球
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      战绳
+                    </Button>
+                  </Col>
+                </Row>
+              </div>
+
+              <div style={{ marginTop: 15 }}>
+                自由器械
+                <Row gutter={16}>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      哑铃
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      杠铃
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      壶铃
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      弹力带
+                    </Button>
                   </Col>
                 </Row>
               </div>
               <div style={{ marginTop: 15 }}>
                 <Row gutter={16}>
-                  <Col className="gutter-row" span={6}>
-                    <Button size='small' type="primary">壶铃</Button>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      瑞士球
+                    </Button>
                   </Col>
-                  <Col className="gutter-row" span={6}>
-                    <Button size='small' type="primary">弹力带</Button>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      泡沫轴
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      波速球
+                    </Button>
+                  </Col>
+                  <Col className="gutter-row" span={4}>
+                    <Button size="small" type="primary">
+                      战绳
+                    </Button>
                   </Col>
                 </Row>
               </div>
