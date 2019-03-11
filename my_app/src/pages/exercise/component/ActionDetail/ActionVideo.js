e27230db260106bc1a06115161684ab2b725c61f
Gao <1520045270@qq.com>
2019-03-11 18:06:29 +0800

    添加视频组件

--- my_app/src/pages/exercise/component/ActionDetail/ActionVideo.js
+++ my_app/src/pages/exercise/component/ActionDetail/ActionVideo.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
-
+import testVideo from "../../../../statics/exercise.mp4";
 class ActionVideo extends Component {
   render() {
-    return <div>这是视频页面</div>;
+    return (
+      <div
+        style={{ border: "1px solid yellow" }}
+      >
+        <video id="my-video" controls preload="auto" width="85%" height="20%">
+          <source src={testVideo} type="video/mp4" />
+        </video>
+      </div>
+    );
   }
 }
 export default ActionVideo;
